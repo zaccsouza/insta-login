a251e50e4dbded681443a482e23b97aac0965980
Zaqueu Souza <Zaqueu.souza@outlook.com>
2021-12-23 21:09:10 -0300

    Tela de Login Instagram.
    Replicando a tela de Login do Intagram.

--- App.js
+++ App.js
@@ -1,21 +1,141 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Platform,
+  KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback} from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+
 
 export default function App() {
   return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor='#FFF' translucent={false} />
+
+     <Image
+      source={require('./src/assets/logo.png')}
+      style={styles.logo}
+      />
+      
+      <TextInput
+       placeholder='Celular, username ou email'
+       style={styles.input}
+      />
+
+      <TextInput
+       placeholder='Sua senha'
+       style={styles.input}
+      />
+
+      <View style={styles.forgotContainer}>
+        <TouchableOpacity>
+          <Text style={styles.forgotText}>Esqueceu sua senha?</Text>
+        </TouchableOpacity>  
+      </View>
+
+      <TouchableOpacity style={styles.loginButton}>
+        <Text style={styles.loginText}>Acessar</Text>
+      </TouchableOpacity>      
+
+      <TouchableOpacity style={styles.facebookContainer }>
+        <FontAwesome5 name='facebook' size={25} color='#399FFF'/>
+        <Text style={styles.facebookText}>Continue como Zaqueu</Text>
+      </TouchableOpacity>
+
+      <View style={styles.divisor}>
+        <View style={styles.divisorLine}></View>
+        <Text style={{marginHorizontal:'3%', color:'#C4C4C4'}}>OU</Text>
+        <View style={styles.divisorLine}></View>
+      </View>
+
+      <View style={styles.signUpContainer}>
+        <Text style={styles.signUpText}>Não possui uma conta?</Text>
+        <TouchableOpacity>
+          <Text style={styles.signUpButton}>Cadastre-se</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
+    </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   backgroundColor: '#FFF',
+   alignItems: 'center',
+   justifyContent: 'center'
   },
-});
+  logo:{
+    marginTop: Platform.OS === 'android' ? '30%' : '20%',
+    marginBottom: Platform.OS === 'android' ? '13%' : '15%',
+  },
+  input:{
+    width: '90%',
+    height: 45,
+    backgroundColor: '#F4F3F3',
+    marginBottom : 20,
+    padding: 8,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor:'#E0E0E0'
+  },
+  forgotContainer:{
+    width: '90%',
+    alignItems: 'flex-end',
+  },
+  forgotText:{
+    color: '#399FFF'
+  },
+  loginButton:{
+    marginTop: '8%',
+    backgroundColor:'#399FFF',
+    width: '90%',
+    height: 45,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5
+  },
+  loginText:{
+    color: '#FFF',
+    fontSize: 16
+  },
+  facebookContainer:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: '15%'
+  },
+  facebookText:{
+    color:'#399FFF',
+    paddingLeft: 8,
+    fontSize: 15
+  },
+  divisor:{
+    marginTop:'10%',
+    flexDirection: 'row',
+    width:'90%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  divisorLine:{
+    width:'45%',
+    height: 2,
+    backgroundColor: '#EFEDED',
+    borderRadius: 6
+  },
+  signUpContainer:{
+    flexDirection:'row',
+    marginTop:'10%',
+  },
+  signUpText:{
+    color:'#C4C4C4',
+    paddingRight: 5,
+  },
+  signUpButton:{
+    color:'#399FFF',
+    fontWeight: 'bold'
+  }
+}); 
